cf68b6f78c76c7099df29b853dca3add125224dd
Centimitr <Centimitr@163.com>
2016-04-10 11:53:16 +0800

    update

--- slices/delete.go
+++ slices/delete.go
@@ -9,13 +9,35 @@ type Interface interface {
 type IntSlice []int
 
 func (s IntSlice) Len() int { return len(s) }
-
 func (s IntSlice) Match(a int, comp interface{}) bool {
 	c := comp.(int)
 	return s[a] == c
 }
-
 func (s IntSlice) Delete(toDelete interface{}) Interface {
+	t := toDelete.([]int)
+	if len(t) > 0 {
+		r := s[:0]
+		lastPos := 0
+		for _, i := range t {
+			r = append(r, s[lastPos:i]...)
+			lastPos = i + 1
+		}
+		if lastPos < len(s) {
+			r = append(r, s[lastPos:len(s)-len(t)+1]...)
+		}
+		return r
+	}
+	return s
+}
+
+type StringSlice []string
+
+func (s StringSlice) Len() int { return len(s) }
+func (s StringSlice) Match(a int, comp interface{}) bool {
+	c := comp.(string)
+	return s[a] == c
+}
+func (s StringSlice) Delete(toDelete interface{}) Interface {
 	t := toDelete.([]int)
 	if len(t) > 0 {
 		r := s[:0]
@@ -41,4 +63,12 @@ func Delete(data Interface, comp interface{}) Interface {
 		}
 	}
 	return data.Delete(toDelete)
+}
+
+func IntDelete(s []int, comp int) []int {
+	return []int(Delete(IntSlice(s), comp).(IntSlice))
+}
+
+func StringDelete(s []string, comp string) []string {
+	return []string(Delete(StringSlice(s), comp).(StringSlice))
 }
